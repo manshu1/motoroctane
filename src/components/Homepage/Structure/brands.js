09e49c6b55d6d914d0a643c48b35a50024f6ab1e
Shrish072003 <shrishsunder444@gmail.com>
2023-08-22 08:10:30 +0530

    first commit12345

--- src/components/Homepage/Structure/brands.js
+++ src/components/Homepage/Structure/brands.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import {useState} from 'react';
 
 
 
 const logos = [
-    { src: 'https://via.placeholder.com/31x31', link: 'https://via.placeholder.com/31x31' },
+    { src: 'hi.png', link: 'https://via.placeholder.com/31x31' },
     { src: 'https://via.placeholder.com/31x31', link: 'https://via.placeholder.com/31x31' },
     { src: 'https://via.placeholder.com/31x31', link: 'https://via.placeholder.com/31x31' },
     { src: 'https://via.placeholder.com/31x31', link: 'https://via.placeholder.com/31x31' },
@@ -40,19 +41,56 @@ const logos = [
     // Add more logos here
     { src: 'https://via.placeholder.com/31x31', link: 'https://via.placeholder.com/31x31' },
   ];
-  
-  const LogosSection = () => (
-    <div className="logos-section">
-      <div className="logos-container">
-        {logos.map((logo, index) => (
-          <a key={index} href={logo.link} target="_blank" rel="noopener noreferrer">
-            <img src={logo.src} alt="Logo" className="logo-image" />
-          </a>
-        ))}
-      </div>
-    </div>
-  );
 
+
+
+
+
+
+
+
+
+  const LogosSection = () => {
+    const logoWidth = 50; // Fixed width of each logo
+    const logosCount = logos.length;
+    const [currentIndex, setCurrentIndex] = useState(0);
+  
+    const handleNext = () => {
+      setCurrentIndex((currentIndex + 1) % logosCount);
+    };
+  
+    const handlePrevious = () => {
+      setCurrentIndex((currentIndex - 1 + logosCount) % logosCount);
+    };
+  
+    const containerStyle = {
+      transform: `translateX(-${currentIndex * logoWidth}px)`,
+    };
+  
+    return (
+        <div className='center_logo'>
+      <section className='brand_logos'>
+        <button className="slider_btn" onClick={handlePrevious}>
+          <ion-icon name="chevron-back-outline"></ion-icon>
+        </button>
+        <div className="logos-section">
+          <div className="logos-container" style={containerStyle}>
+            {logos.map((logo, index) => (
+              <a key={index} href={logo.link} target="_blank" rel="noopener noreferrer">
+                <img src={logo.src} alt="Logo" className="logo-image" />
+              </a>
+            ))}
+          </div>
+        </div>
+        <button className="slider_btn" onClick={handleNext}>
+          <ion-icon name="chevron-forward-outline"></ion-icon>
+        </button>
+      </section>
+      </div>
+    );
+  };
+  
+  
 
 
 const brands = () => {
