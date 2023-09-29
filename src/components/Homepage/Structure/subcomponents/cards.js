f68abbd00ae68052c9b233e8d8579d0ed40d28ef
rajanagh <132365124+rajanagh@users.noreply.github.com>
2023-09-29 14:58:16 +0530

    bug fix moto

--- src/components/Homepage/Structure/subcomponents/cards.js
+++ src/components/Homepage/Structure/subcomponents/cards.js
@@ -29,7 +29,31 @@ const Cards = () => {
             transform: `translateX(-${currentIndex * cardWidth}px)`, // Slide animation
           }}>
         <div key={card.id} className='main_card'>
-          <div className="bookmarkRibbon"></div>
+          <div className="bookmarkRibbon">  <div>
+    <div>
+      <div>
+        <svg
+          aria-label="Save"
+          class="x1lliihq"
+          color="gray"
+          fill="rgb(245, 245, 245)"
+          height="40"
+          role="img"
+          viewBox="0 0 24 40"
+          width="24"
+        >
+          <title>Save</title>
+          <polygon
+            points="20 21 12 13.44 4 21 4 3 20 3 20 21"
+            stroke="currentColor"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="1"
+          ></polygon>
+        </svg>
+      </div>
+    </div>
+  </div></div>
           <div className="inside_card">
           <div className="inside_card_title">{card.title}<span>{card.title2}</span></div>
           <img className="main_card_image" src={card.imageSrc} alt={card.title} />
@@ -44,10 +68,14 @@ const Cards = () => {
         </div>
      
       <section className='main_card_info'>
-        <div className='side_info'> <img className='icon_image' src={seater} alt="Seater Icon" /> Seater info</div>
-        <div className='side_info'> <img className='icon_image' src={petrol} alt="Petrol Icon" /> Petrol info</div>
-        <div className='side_info'> <img className='icon_image' src={manul} alt="Manual Icon" /> Manual info</div>
-        <div className='side_info'> <img className='icon_image' src={ncap} alt="NCAP Icon" />NCAP info</div>
+        <div className='side_info'> <img className='icon_image' src={seater} alt="Seater Icon" /><div className='side_info_inline'><span>5 Seater
+</span><span>7 Seater</span></div></div>
+        <div className='side_info'> <img className='icon_image' src={petrol} alt="Petrol Icon" /> <div className='side_info_inline'><span>Petrol | Diesel
+</span><span>Electric | Hybrid</span></div></div>
+        <div className='side_info'> <img className='icon_image' src={manul} alt="Manual Icon" /> <div className='side_info_inline'><span>Manual | Auto
+</span><span>DCT | Etc</span></div></div>
+        <div className='side_info'> <img className='icon_image' src={ncap} alt="NCAP Icon" /><div className='side_info_inline'><span>NCAP:
+</span><span>5 Star</span></div></div>
       </section>
     </section> ))}
     </div>
